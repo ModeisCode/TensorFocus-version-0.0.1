a4260531cb84f0ba27964e908ec3b643e3a2704d
SoftwareArchitecture <100340196+ModeisCode@users.noreply.github.com>
2024-05-02 23:04:28 +0300

    Update and rename tensorLib.cpp to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,12 @@
-﻿
-
 #include <iostream>
 #include "Tensor.h"
 
 int main()
 {
-
 	Size size;
 	size.x = 3;
 	size.y = 1;
 
 	Tensor<int> tensor(size);
 	std::cout << "Tensor_data:" << tensor.getData().at(0).at(0) << std::endl;
-
-
 }
-
-
-	/*
-	Size size;
-	size.x = 2;
-	size.y = 2;
-	Tensor<int> tensor(size , "integer");
-	auto data = tensor.getData();
-	std::cout << "Tensor_data:" << data.at(0).at(0) << std::endl;
-	*/
